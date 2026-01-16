a2362894ca07cf5c88ea01c78230351a7bcdd9d8
Tomasz Jatkowski <tomasz.jatkowski@u2i.com>
2026-01-16 14:43:14 +0100

    Fix shell_out for Chef 18: use array form with explicit bash

--- libraries/provider_rvm_installation.rb
+++ libraries/provider_rvm_installation.rb
@@ -104,7 +104,7 @@ class Chef
       end
 
       def run_install_cmd
-        rvm_shell_out!(%{/bin/bash #{rvm_installer_path} #{new_resource.installer_flags}})
+        rvm_shell_out!(%{bash #{rvm_installer_path} #{new_resource.installer_flags}})
       end
 
       def rvm_installer_path
@@ -151,7 +151,8 @@ class Chef
         }
 
         Chef::Log.debug("Running [#{cmd}] with #{opts}")
-        shell_out(cmd, opts)
+        # Chef 18 shell_out needs explicit shell invocation when using options
+        shell_out("/bin/bash", "-c", cmd, opts)
       end
 
       def rvm_shell_out!(*args)
