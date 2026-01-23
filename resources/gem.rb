7b084adcf2002c7ba9ff6f86987761a771dd13a1
mzawislak <maksymilian.zawislak@u2i.com>
2026-01-23 15:14:58 +0100

    Add include_default_source attribute for Chef 18 compatibility

--- resources/gem.rb
+++ resources/gem.rb
@@ -29,6 +29,7 @@ attribute :source,        :kind_of => String
 attribute :options,       :kind_of => Hash
 attribute :gem_binary,    :kind_of => String
 attribute :user,          :kind_of => String
+attribute :include_default_source, :kind_of => [TrueClass, FalseClass], :default => true
 
 def initialize(*args)
   super
