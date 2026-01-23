e55d7cc3a3643942eb5d1a89086d74e4cc8ae3ec
mzawislak <maksymilian.zawislak@u2i.com>
2026-01-23 15:16:11 +0100

    Add environment attribute for Chef 18 compatibility

--- resources/gem.rb
+++ resources/gem.rb
@@ -30,6 +30,7 @@ attribute :options,       :kind_of => Hash
 attribute :gem_binary,    :kind_of => String
 attribute :user,          :kind_of => String
 attribute :include_default_source, :kind_of => [TrueClass, FalseClass], :default => true
+attribute :environment,   :kind_of => Hash
 
 def initialize(*args)
   super
